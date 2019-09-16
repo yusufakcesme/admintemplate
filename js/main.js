7ec23dc5fbccec4c9a749fb3c5bdf1e0145e2009
yusufakcesme <yusufakcesme456@gmail.com>
2019-09-16 22:54:00 +0300

    v2.0 released

--- js/main.js
+++ js/main.js
@@ -20,5 +20,16 @@ function profileOn() {
 
 }
 
+/*
 
+function datee() {
+	var today = new Date();
+	var dd = String(today.getDate()).padStart(2, '0');
+	var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+	var yyyy = today.getFullYear();!''
+	today = dd + '/' + mm + '/' + yyyy;
+
+	document.write(today);
+}
+*/
 
