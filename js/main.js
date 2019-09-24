4a64d3bb2f6c66ba6318136000508e889ac0757b
yusufakcesme <yusufakcesme456@gmail.com>
2019-09-24 21:46:23 +0300

    v2.4 release

--- js/main.js
+++ js/main.js
@@ -19,14 +19,49 @@ function profileOn() {
 	}
 
 }
+//-------------------------------------------------------
+
+//--------------------------------------------------------
 
 var nav_btn = document.querySelectorAll('.nav_btn');
+var info_section = document.getElementById('info-section');
+var panel_form = document.getElementById('panel-form');
+var hakkinda_panel = document.getElementById("hakkinda-panel");
 
-function activeBtn() {
+function activeButton() {
 	removeActive();
-	this.classList.add("active");
+	this.classList.add('active');
+
+	if (this.id === "anasayfa") {
+		info_section.style.display = "flex";
+		panel_form.style.display = "none";
+		hakkinda_panel.style.display = "none";
+	} else if (this.id === "ayarlar") {
+		info_section.style.display = "none";
+		panel_form.style.display = "block";
+		hakkinda_panel.style.display = "none";
+	} else if (this.id === "hakkinda") {
+		info_section.style.display = "none";
+		panel_form.style.display = "none";
+		hakkinda_panel.style.display = "block";
+	}
+
+
+
+
 }
+
 function removeActive() {
-	nav_btn.forEach(item => item.classList.remove("active"));
+
+	nav_btn.forEach(item => item.classList.remove('active'));
 }
-nav_btn.forEach(item => item.addEventListener('click', activeBtn));
+
+
+
+
+
+nav_btn.forEach(item => item.addEventListener('click', activeButton));
+
+
+
+
