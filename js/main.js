74294e70beb97acab5585311284214b9177a2c91
yusufakcesme <yusufakcesme456@gmail.com>
2019-09-29 00:17:52 +0300

    v2.6 release

--- js/main.js
+++ js/main.js
@@ -27,40 +27,37 @@ var nav_btn = document.querySelectorAll('.nav_btn');
 var info_section = document.getElementById('info-section');
 var panel_form = document.getElementById('panel-form');
 var hakkinda_panel = document.getElementById("hakkinda-panel");
+var page_name = document.getElementById('page-name');
 
 function activeButton() {
 	removeActive();
 	this.classList.add('active');
 
 	if (this.id === "anasayfa") {
+		page_name.innerText = "Anasayfa";
 		info_section.style.display = "flex";
 		panel_form.style.display = "none";
 		hakkinda_panel.style.display = "none";
 	} else if (this.id === "ayarlar") {
+		page_name.innerText = "Ayarlar";
 		info_section.style.display = "none";
 		panel_form.style.display = "block";
 		hakkinda_panel.style.display = "none";
 	} else if (this.id === "hakkinda") {
+		page_name.innerText = "Hakkında";
 		info_section.style.display = "none";
 		panel_form.style.display = "none";
 		hakkinda_panel.style.display = "block";
 	}
-
-
-
-
 }
-
 function removeActive() {
-
 	nav_btn.forEach(item => item.classList.remove('active'));
 }
 
-
-
-
-
 nav_btn.forEach(item => item.addEventListener('click', activeButton));
+
+
+/*-------------------------------------------------*/
 
 
 
